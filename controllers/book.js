49a1748ea0c6a71f260902d03d09d949c6f41f7d
ABIDULLAH786 <abidullah.bsef18@iba-suk.edu.pk>
2022-05-20 22:27:52 +0500

    Backend Added

--- controllers/book.js
+++ controllers/book.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex")
 module.exports.getBooks = async (req, res) => {
     try {
         const books = await knex('books').select('*');
-        
+
         res.status(200).json({
             success: true,
             books: books
@@ -15,7 +15,7 @@ module.exports.getBooks = async (req, res) => {
 
 exports.findBookById = async (req, res, next) => {
     try {
-        const bookDetail = await knex('books').where('id',req.params.id).select("*");
+        const bookDetail = await knex('books').where('id', req.params.id).select("*");
         res.status(200).json({
             success: true,
             bookDetail: bookDetail
